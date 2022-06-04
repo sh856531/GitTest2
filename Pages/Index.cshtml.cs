1eaa294a0cbefeb89fc491acb70a4341f9e39a72
sh856531 <sh856531@hotmail.com>
2022-06-04 14:59:28 +0100

    JQuery Update

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,6 +5,10 @@ namespace GitTest2.Pages
 {
     public class IndexModel : PageModel
     {
+
+        private readonly string username = "myUsername";
+        private readonly string password = "myPassword";
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
