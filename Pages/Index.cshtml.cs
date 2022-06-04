b0a4eb6210e5747af8be6988ddc910d74e929736
sh856531 <sh856531@hotmail.com>
2022-06-04 15:21:13 +0100

    Added azure connection string

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -8,7 +8,8 @@ namespace GitTest2.Pages
 
         private readonly string username = "myUsername";
         private readonly string password = "myPassword";
-        string connectionString = "Data Source=localhost;Database=secretDatabase;Integrated Security=false;User ID=administrator;Password=secretpassword;"
+        string connectionString = "Data Source=localhost;Database=secretDatabase;Integrated Security=false;User ID=administrator;Password=secretpassword;";
+        string azureConnectionString = "Server = tcp:myserver.database.windows.net,1433;Database=myDataBase;User ID = mylogin@myserver;Password=myPassword;Trusted_Connection=False;Encrypt=True;";
 
         private readonly ILogger<IndexModel> _logger;
 
